1da93c138f3c65b2fca4c194efe6ae42c9750d75
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-08-08 09:41:17 +0545

    naya kura ho

--- src/components/CardButton.jsx
+++ src/components/CardButton.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const CardButton = () => {
+const CardButton = ({ color }) => {
+  // const age = 90;
+  // const nameSen = 'hello jee how r u';
+
+  // console.log(`sdkfsdkf ${age} ${nameSen}`);
+
   return (
     <div>
 
-      <button className='bg-gray-600 text-white px-2 py-1 rounded-sm hover:ring-2 hover:ring-pink-900'>Learn More</button>
+      <button className={`${color} text-white px-2 py-1 rounded-sm hover:ring-2 hover:ring-pink-900`}>Learn More</button>
     </div>
   )
 }
--- src/components/CardCompo.jsx
+++ src/components/CardCompo.jsx
@@ -10,7 +10,9 @@ const CardCompo = ({ title, detail, url }) => {
       <img src={url} alt="" />
       <h3>{title}</h3>
       <p>{detail}</p>
-      <CardButton />
+      <CardButton color={'bg-red-500'} />
+      <CardButton color={'bg-green-500'} />
+      <CardButton color={'bg-blue-500'} />
 
     </div>
   )

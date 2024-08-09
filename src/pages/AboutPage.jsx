aee1b5771ed22347183f99ebbac7b77afc3e002e
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-08-09 09:03:13 +0545

    ch

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const AboutPage = () => {
   return (
     <div>
+      <Header />
       <h1>THis is About Page</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam impedit, saepe quas magnam quibusdam corrupti voluptatem? Atque non, recusandae in totam velit, ab molestiae necessitatibus et, minima pariatur delectus obcaecati.</p>
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const HomePage = () => {
   return (
     <div>
+      <Header />
       <h1>This is home page</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint vel, ut delectus quis et praesentium iste quia alias officia perspiciatis architecto rem magni voluptatem, ab cum hic recusandae modi laudantium!</p>
     </div>

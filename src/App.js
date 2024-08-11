9e689e1194fbe43fe2592195eeeda7532213b3c5
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-08-11 08:34:24 +0545

    new upadates

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
-import Sample1 from './pages/Sample1';
-import Sample2 from './pages/Sample2';
+import RootLayout from './components/RootLayout';
 
 const App = () => {
 
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <HomePage />,
+      element: <RootLayout />,
       children: [
         {
-          path: 'sample1',
-          element: <Sample1 />
+          path: 'about-page',
+          element: <AboutPage />
         },
         {
-          path: 'sample2',
-          element: <Sample2 />
+          path: 'contact-page',
+          element: <ContactPage />
         }
       ]
     },
 
 
-    {
-      path: 'about-page',
-      element: <AboutPage />
-    },
-    {
-      path: 'contact-page',
-      element: <ContactPage />
-    }
+
   ]);
 
 

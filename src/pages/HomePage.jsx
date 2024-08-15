acb041aebdaf246aec3f010e8ec413f9cb8c4ada
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-08-15 09:31:42 +0545

    all file update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,8 @@
-
-
 import React from 'react'
-import DisplaySection from './home/DisplaySection'
-import Tech from './home/Tech'
-import Info from './home/Info'
 
 const HomePage = () => {
   return (
-    <div className='px-[20px]'>
-
-
-
-
-      <DisplaySection />
-      <Tech />
-      <Info />
-
+    <div>
 
 
     </div>

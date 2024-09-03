ee4c034157e5d5b390aa13019ee64f5f9fec4a95
Rabyn-Collab <babynshrestha64@gmail.com>
2024-09-03 09:20:38 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -1,49 +1,6 @@
-import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import RootLayout from './components/RootLayout';
-import HomePage from './pages/HomePage';
-import ItemsPages from './pages/ItemsPages';
-import ItemDetail from './pages/ItemDetail';
-//
 const App = () => {
-
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
-          element: <HomePage />
-        },
-        {
-          path: 'item/:category',
-          element: <ItemsPages />
-        },
-        {
-          path: 'item-detail/:id',
-          element: <ItemDetail />
-        },
-        {
-          path: 'about-page',
-          element: <AboutPage />
-        },
-        {
-          path: 'contact-page',
-          element: <ContactPage />
-        }
-      ]
-    },
-
-
-
-  ]);
-
-
-
-  return <RouterProvider router={router} />
+  return (
+    <div>App</div>
+  )
 }
-
 export default App

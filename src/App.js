f125f64a5552a65f636fbe704c42c0103e21d718
Rabyn-Collab <babynshrestha64@gmail.com>
2024-09-03 10:12:06 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import RootLayout from "./components/RootLayout";
+import Sam from "./pages/Sam";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -16,7 +17,14 @@ const App = () => {
         },
         {
           path: "about-page",
-          element: <About />
+          element: <About />,
+          children: [
+            {
+              path: 'sam',
+              element: <Sam />
+            }
+
+          ]
         },
         {
           path: "contact-page",
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router"
+import { NavLink } from "react-router-dom"
+
 const About = () => {
   return (
     <div>
 
       <h1>This is about page</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate cum voluptatibus totam consequuntur accusantium. Fuga laboriosam aperiam, laborum reiciendis, alias nihil rerum deleniti quaerat magnam voluptates libero mollitia sapiente! Omnis?</p>
+      <NavLink to={'/about-page/sam'}>sam</NavLink>
+      <Outlet />
     </div>
   )
 }

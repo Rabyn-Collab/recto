7c810f2dd99ee0bd49cb9417aa667401d3626e33
Rabyn-Collab <babynshrestha64@gmail.com>
2024-09-27 10:16:12 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./app/appRoute"
+import { useGetPostsQuery } from "./features/todos/todosApi"
 
 
 const App = () => {
+
+  const { isLoading, data, error, isError } = useGetPostsQuery();
+
+
+  // const f = () => ({
+  //   name: 'ram'
+  // });
+
+  // const c = f();
+
+  // console.log(c);
+
   // const arrs = [{ id: 1, name: 'ram', }, { id: 2, name: 'krishna' }];
 
   // const n = arrs.map((a) => {

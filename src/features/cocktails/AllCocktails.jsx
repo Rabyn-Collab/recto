45b5c2cbade9112a7fa72f4d8ba2e4d8651fd047
Rabyn-Collab <babynshrestha64@gmail.com>
2024-09-30 10:01:25 +0545

    ch

--- src/features/cocktails/AllCocktails.jsx
+++ src/features/cocktails/AllCocktails.jsx
@@ -4,13 +4,20 @@ const AllCocktails = () => {
 
   const { isLoading, isError, isFetching, data } = useGetAllDrinksQuery();
 
+  const [fetchAll, { isLoading: isLoad, data: val, error }] = useLazyGetAllDrinksQuery();
 
+  if (isLoading) {
+    return <h1>Loading....</h1>
 
-
-
-
+  }
   return (
-    <div>
+    <div className="p-3">
+
+      {
+        data && data.drinks.map((cocktail, i) => {
+          return <h1 className="shadow-lg p-2 cursor-pointer mb-4" key={i}>{cocktail.strCategory}</h1>
+        })
+      }
 
     </div>
   )
